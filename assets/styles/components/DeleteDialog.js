0278dec8647a70fe8c1fe2c4e6e44183158669ae
Camilo <camilo@tipddy.cl>
2021-06-23 09:43:13 -0400

    delete dialog fixed

--- assets/styles/components/DeleteDialog.js
+++ assets/styles/components/DeleteDialog.js
@@ -1,31 +1,37 @@
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import PropTypes from 'prop-types';
 import React, {useContext} from 'react';
-import {Button, DialogActions, DialogContent, DialogTitle} from "@material-ui/core";
-import PropTypes from "prop-types";
-import {Dialog} from "@material-ui/core";
 import {TodoContext} from "../Context/TodoContext";
+
 
 function DeleteDialog(props) {
     const context = useContext(TodoContext);
+
     const hide = () => {
-        props.setDeleteConfirmIsShown(false);
-    }
-        return (
-            <Dialog onClose={hide} fullWidth={true} maxWidth='sm' open={props.open}>
-                <DialogTitle>Are you sure to delete this task?</DialogTitle>
+        props.setDeleteConfirmationIsShown(false);
+    };
+
+    return (
+        <Dialog onClose={hide} fullWidth={true} maxWidth='sm' open={props.open}>
+            <DialogTitle>Are you sure you wish to delete this to-do?</DialogTitle>
             <DialogContent>
                 {props.todo.name}
             </DialogContent>
-                <DialogActions>
-                    <Button onClick={hide}>Cancel</Button>
-                    <Button onClick={()=>{
-                        context.deleteTodo({id: props.todo.id, name: props.todo.name});
-                        hide();
-                    }}>
-                        Delete
-                    </Button>
-                </DialogActions>
-            </Dialog>
-        );
+            <DialogActions>
+                <Button onClick={hide}>Cancel</Button>
+                <Button onClick={() => {
+                    context.deleteTodo({id: props.todo.id, name: props.todo.name});
+                    hide();
+                }}>
+                    Delete
+                </Button>
+            </DialogActions>
+        </Dialog>
+    );
 }
 
 DeleteDialog.propTypes = {
